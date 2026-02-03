15cf9d6e44a6ec5fb250b88e4919aec6ed1d4cac
Victor Antonovich <v.antonovich@gmail.com>
2026-02-03 14:38:06 +0300

    Translate wx_meteo module messages to Russian

--- modules/wx_meteo.py
+++ modules/wx_meteo.py
@@ -45,10 +45,10 @@ def get_wx_meteo(lat=0, lon=0, unit=0):
 	try:
 		# Process location
 		logger.debug(f"System: Pulled from Open-Meteo in {weather_data['timezone']} {weather_data['timezone_abbreviation']}")
-		
+
 		# Ensure response is defined
 		response = weather_data
-		
+
 		# Process daily data. The order of variables needs to be the same as requested.
 		daily = response['daily']
 		daily_weather_code = daily['weather_code']
@@ -66,128 +66,128 @@ def get_wx_meteo(lat=0, lon=0, unit=0):
 	# convert wind value to cardinal directions
 	for value in daily_wind_direction_10m_dominant:
 		if value < 22.5:
-			wind_direction = "N"
+			wind_direction = "С"
 		elif value < 67.5:
-			wind_direction = "NE"
+			wind_direction = "СВ"
 		elif value < 112.5:
-			wind_direction = "E"
+			wind_direction = "В"
 		elif value < 157.5:
-			wind_direction = "SE"
+			wind_direction = "ЮВ"
 		elif value < 202.5:
-			wind_direction = "S"
+			wind_direction = "Ю"
 		elif value < 247.5:
-			wind_direction = "SW"
+			wind_direction = "ЮЗ"
 		elif value < 292.5:
-			wind_direction = "W"
+			wind_direction = "З"
 		elif value < 337.5:
-			wind_direction = "NW"
+			wind_direction = "СЗ"
 		else:
-			wind_direction = "N"
+			wind_direction = "С"
 
 	# create a weather report
 	weather_report = ""
 	for i in range(forecastDays):
 		if str(i + 1) == "1":
-			weather_report += "Today, "
+			weather_report += "Сегодня: "
 		elif str(i + 1) == "2":
-			weather_report += "Tomorrow, "
+			weather_report += "Завтра: "
 		else:
-			weather_report += "Futurecast: "
-		
+			weather_report += "Прогноз: "
+
 		# report weather from WMO Weather interpretation codes (WW)
 		code_string = ""
 		if daily_weather_code[i] == 0:
-			code_string = "Clear sky"
+			code_string = "Ясно"
 		elif daily_weather_code[i] == 1:
-			code_string = "Mostly Cloudy"
+			code_string = "Преимущественно облачно"
 		elif daily_weather_code[i] == 2:
-			code_string = "Partly Cloudy"
+			code_string = "Переменная облачность"
 		elif daily_weather_code[i] == 3:
-			code_string = "Overcast"
+			code_string = "Пасмурно"
 		elif daily_weather_code[i] == 5:
-			code_string = "Haze"
+			code_string = "Мгла"
 		elif daily_weather_code[i] == 10:
-			code_string = "Mist"
+			code_string = "Дымка"
 		elif daily_weather_code[i] == 45:
-			code_string = "Fog"
+			code_string = "Туман"
 		elif daily_weather_code[i] == 48:
-			code_string = "Freezing Fog"
+			code_string = "Замерзающий туман"
 		elif daily_weather_code[i] == 51:
-			code_string = "Drizzle: Light"
+			code_string = "Слабая морось"
 		elif daily_weather_code[i] == 53:
-			code_string = "Drizzle: Moderate"
+			code_string = "Умеренная морось"
 		elif daily_weather_code[i] == 55:
-			code_string = "Drizzle: Heavy"
+			code_string = "Сильная морось"
 		elif daily_weather_code[i] == 56:
-			code_string = "Freezing Drizzle: Light"
+			code_string = "Слабая замерзающая морось"
 		elif daily_weather_code[i] == 57:
-			code_string = "Freezing Drizzle: Moderate"
+			code_string = "Умеренная замерзающая морось"
 		elif daily_weather_code[i] == 61:
-			code_string = "Rain: Slight"
+			code_string = "Слабый дождь"
 		elif daily_weather_code[i] == 63:
-			code_string = "Rain: Moderate"
+			code_string = "Умеренный дождь"
 		elif daily_weather_code[i] == 65:
-			code_string = "Rain: Heavy"
+			code_string = "Сильный дождь"
 		elif daily_weather_code[i] == 66:
-			code_string = "Freezing Rain: Light"
+			code_string = "Слабый ледяной дождь"
 		elif daily_weather_code[i] == 67:
-			code_string = "Freezing Rain: Dense"
+			code_string = "Сильный ледяной дождь"
 		elif daily_weather_code[i] == 71:
-			code_string = "Snow: Light"
+			code_string = "Слабый снег"
 		elif daily_weather_code[i] == 73:
-			code_string = "Snow: Moderate"
+			code_string = "Умеренный снег"
 		elif daily_weather_code[i] == 75:
-			code_string = "Snow: Heavy"
+			code_string = "Сильный снег"
 		elif daily_weather_code[i] == 77:
-			code_string = "Snow Grains"
+			code_string = "Снежная крупа"
 		elif daily_weather_code[i] == 78:
-			code_string = "Ice Crystals"
+			code_string = "Изморось"
 		elif daily_weather_code[i] == 79:
-			code_string = "Ice Pellets"
+			code_string = "Ледяная крупа"
 		elif daily_weather_code[i] == 80:
-			code_string = "Rain showers: Slight"
+			code_string = "Слабый ливневый дождь"
 		elif daily_weather_code[i] == 81:
-			code_string = "Rain showers: Moderate"
+			code_string = "Умеренный ливневый дождь"
 		elif daily_weather_code[i] == 82:
-			code_string = "Rain showers: Heavy"
+			code_string = "Сильный ливневый дождь"
 		elif daily_weather_code[i] == 85:
-			code_string = "Snow showers"
+			code_string = "Ливневый снегопад"
 		elif daily_weather_code[i] == 86:
-			code_string = "Snow showers: Heavy"
+			code_string = "Сильный ливневый снегопад"
 		elif daily_weather_code[i] == 95:
-			code_string = "Thunderstorm"
+			code_string = "Гроза"
 		elif daily_weather_code[i] == 96:
-			code_string = "Hailstorm"
+			code_string = "Град"
 		elif daily_weather_code[i] == 97:
-			code_string = "Thunderstorm Heavy"
+			code_string = "Сильная гроза"
 		elif daily_weather_code[i] == 99:
-			code_string = "Hailstorm Heavy"
+			code_string = "Сильный град"
 
-		weather_report += "Cond: " + code_string + ". "
+		weather_report += code_string + ". "
 
 		# report temperature
 		if unit == 0:
-			weather_report += "High: " + str(int(round(daily_temperature_2m_max[i]))) + "F, with a low of " + str(int(round(daily_temperature_2m_min[i]))) + "F. "
+			weather_report += "Макс: " + str(int(round(daily_temperature_2m_max[i]))) + "F, мин " + str(int(round(daily_temperature_2m_min[i]))) + "F. "
 		else:
-			weather_report += "High: " + str(int(round(daily_temperature_2m_max[i]))) + "C, with a low of " + str(int(round(daily_temperature_2m_min[i]))) + "C. "
+			weather_report += "Макс: " + str(int(round(daily_temperature_2m_max[i]))) + "C, мин " + str(int(round(daily_temperature_2m_min[i]))) + "C. "
 
 		# check for precipitation
 		if daily_precipitation_hours[i] > 0:
 			if unit == 0:
-				weather_report += "Precip: " + str(round(daily_precipitation_probability_max[i],2)) + "in, in " + str(round(daily_precipitation_hours[i],2)) + " hours. "
+				weather_report += "Осадки: " + str(round(daily_precipitation_probability_max[i],2)) + " дм, через " + str(round(daily_precipitation_hours[i],2)) + " ч. "
 			else:
-				weather_report += "Precip: " + str(round(daily_precipitation_probability_max[i],2)) + "mm, in " + str(round(daily_precipitation_hours[i],2)) + " hours. "
+				weather_report += "Осадки: " + str(round(daily_precipitation_probability_max[i],2)) + " мм, через " + str(round(daily_precipitation_hours[i],2)) + " ч. "
 		else:
-			weather_report += "No Precip. "
+			weather_report += "Без осадков. "
 
 		# check for wind
 		if daily_wind_speed_10m_max[i] > 0:
 			if unit == 0:
-				weather_report += "Wind: " + str(int(round(daily_wind_speed_10m_max[i]))) + "mph, gusts up to " + str(int(round(daily_wind_gusts_10m_max[i]))) + "mph from:" + wind_direction + "."
+				weather_report += "Ветер: " + str(int(round(daily_wind_speed_10m_max[i]))) + "миль/ч, порывы " + str(int(round(daily_wind_gusts_10m_max[i]))) + " миль/ч, направление: " + wind_direction + "."
 			else:
-				weather_report += "Wind: " + str(int(round(daily_wind_speed_10m_max[i]))) + "kph, gusts up to " + str(int(round(daily_wind_gusts_10m_max[i]))) + "kph from:" + wind_direction + "."
+				weather_report += "Ветер: " + str(int(round(daily_wind_speed_10m_max[i]))) + "км/ч, порывы " + str(int(round(daily_wind_gusts_10m_max[i]))) + "км/ч направление: " + wind_direction + "."
 		else:
-			weather_report += "No Wind\n"
+			weather_report += "Штиль\n"
 
 		# add a new line for the next day
 		if i < forecastDays - 1:
@@ -215,15 +215,15 @@ def get_flood_openmeteo(lat=0, lon=0):
 	except Exception as e:
 		logger.error(f"Error fetching meteo flood data: {e}")
 		return ERROR_FETCHING_DATA
-	
+
 	# Check if we got a response
 	try:
 		# Process location
 		logger.debug(f"System: Pulled River FLow Data from Open-Meteo {flood_data['timezone_abbreviation']}")
-		
+
 		# Ensure response is defined
 		response = flood_data
-		
+
 		# Process daily data. The order of variables needs to be the same as requested.
 		daily = response['daily']
 		daily_river_discharge = daily['river_discharge']
@@ -232,9 +232,9 @@ def get_flood_openmeteo(lat=0, lon=0):
 	except Exception as e:
 		logger.error(f"Error processing meteo flood data: {e}")
 		return ERROR_FETCHING_DATA
-	
+
 	# create a flood report
 	flood_report = ""
-	flood_report += "River Discharge: " + str(daily_river_discharge) + "m3/s"
+	flood_report += "Сброс воды: " + str(daily_river_discharge) + "м3/с"
 
 	return flood_report
